43dbd553c05a9853aeaba9bf0e7ff845b33282d4
NalinPriyankara <nalinpriyankara033@gmail.com>
2025-07-29 23:06:59 +0530

    backend complete

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,6 +16,7 @@ app.use(cors({credentials: true}));
 
 //api endpoints
 app.get('/', (req, res) => res.send('API is working'));
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
